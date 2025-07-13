6d1da659a98115dfd6db8633505f2412d5add954
SriHarshaRajuY <sriharsharajuy@gmail.com>
2025-07-13 17:59:46 +0530

    Job Listing Component added

---
+++ client/src/components/AppDownload.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AppDownload = () => {
+return (
+    <div>
+        <div>
+            <div>
+                <h1>Download Mobile App for Better Experience</h1>
+                <div>
+                    <a href="">
+                        <img src={assets.play_store} alt="" />
+                    </a>
+                    <a href="">
+                        <img src={assets.app_store} alt=""/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default AppDownload
