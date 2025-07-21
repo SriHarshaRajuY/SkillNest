5d9677fd9bcb2f783f2c2ef257264b13bb8d8af0
SriHarshaRajuY <sriharsharajuy@gmail.com>
2025-07-21 20:18:35 +0530

    Applications page updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {assets} from '../assets/assets'
 import {useClerk, UserButton, useUser} from '@clerk/clerk-react'
 
@@ -8,10 +8,12 @@ const Navbar = () => {
     const {openSignIn} = useClerk();
     const {user} = useUser();
 
+    const navigate = useNavigate()
+
 return (
     <div className='shadow py-4'>
         <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
-            <img src={assets.logo} alt="" />
+            <img onClick={() => navigate('/')} className='cursor-pointer' src={assets.logo} alt="" />
             {
                 user
                 ?<div className='flex items-center gap-3'>
