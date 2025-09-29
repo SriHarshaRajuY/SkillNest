914ce5205f04daaf5901e3d1eadec28b3a6b23ea
SriHarshaRajuY <sriharsharajuy@gmail.com>
2025-09-30 00:56:22 +0530

    Dashboard added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Dashboard = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>Dashboard</div>
+        <div className='min-h-screen'>
+            {/*Navbar for Recruiter Panel */}
+            <div className='shadow py-4'>
+                <div className='px-5 flex justify-between items-center'>
+                    <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt=""/>
+                    <div className='flex items-center gap-3'>
+                        <p className='max-sm:hidden'>Welcome, Stack</p>
+                        <div className='relative group'>
+                            <img className='w-8 border rounded-full' src={assets.company_icon} alt=""/>
+                            <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                                <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                                    <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex items-start'>
+                {/* Left sidebar with option to add job, manage jobs, view applications */}
+                <div className='inline-block min-h-screen border-r-2'>
+                    <ul className='flex flex-col items-start pt-5 text-gray-800'>
+                        <NavLink to={'/dashboard/add-job'}>
+                            <img src={assets.add_icon} alt=""/>
+                            <p>Add Job</p>
+                        </NavLink>
+
+                        <NavLink to={'/dashboard/manage-jobs'}>
+                            <img src={assets.home_icon} alt=""/>
+                            <p>Manage Jobs</p>
+                        </NavLink>
+
+                        <NavLink to={'/dashboard/view-applications'}>
+                            <img src={assets.person_tick_icon} alt=""/>
+                            <p>View Applications</p>
+                        </NavLink>
+                    </ul>
+                </div>
+
+
+            </div>
+
+        </div>
     )
 }
 
