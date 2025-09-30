b5eac818c8133402ef9b221adf30e6a69324f15f
SriHarshaRajuY <sriharsharajuy@gmail.com>
2025-09-30 21:46:36 +0530

    dashboard/add-job completed till dropdowns

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,35 @@
-import React, { useContext } from 'react'
-  import { Route, Routes } from 'react-router-dom'
-  import ApplyJob from './pages/Applyjob'
-  import Home from './pages/Home'
-  import Applications from './pages/Applications'
-import RecruiterLogin from './components/RecruiterLogin'
-import { AppContext } from './context/AppContext'
-import AddJob from './pages/AddJob'
-import Dashboard from './pages/Dashboard'
-import ManageJobs from './pages/ManageJobs'
-import ViewApplications from './pages/ViewApplications'
+import React, { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ApplyJob from './pages/Applyjob';
+import Home from './pages/Home';
+import Applications from './pages/Applications';
+import RecruiterLogin from './components/RecruiterLogin';
+import { AppContext } from './context/AppContext';
+import AddJob from './pages/AddJob';
+import Dashboard from './pages/Dashboard';
+import ManageJobs from './pages/ManageJobs';
+import ViewApplications from './pages/ViewApplications';
+import 'quill/dist/quill.snow.css';
 
-  const App = () => {
+const App = () => {
+  const { showRecruiterLogin } = useContext(AppContext);
+  return (
+    <div>
+      {showRecruiterLogin && <RecruiterLogin />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Applications />} />
+        {/* Nest all dashboard-related routes here */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          {/* These are relative paths: /dashboard/add-job, etc. */}
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
-    const {showRecruiterLogin} = useContext(AppContext)
-    return (
-      <div>
-        {showRecruiterLogin && <RecruiterLogin/>}
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/apply-job/:id' element={<ApplyJob/>}/>
-          <Route path='/applications' element={<Applications/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='add-job' element={<AddJob/>}/>
-          <Route path='manage-jobs' element={<ManageJobs/>}/>
-          <Route path='view-applications' element={<ViewApplications/>}/>
-        </Routes>
-      </div>
-    )
-  }
-
-  export default App
+export default App;
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Quill from 'quill';
 
 const AddJob = () => {
+
+    const [title, setTitle] = useState('');
+    const [location, setLocation] = useState('Banglore');
+    const [category, setCategory] = useState('Programming');
+    const [level, setLevel] = useState('Beginner Level');
+    const [salary, setSalary] = useState(0);
+
+    const editorRef = useRef(null);
+    const quillRef = useRef(null);
+
+    useEffect(() => {
+        //Initiate Quill only once
+        if(!quillRef.current && editorRef.current) {
+            quillRef.current = new Quill(editorRef.current, {
+                theme: 'snow',
+
+            })
+        }
+    }, [])
+
     return (
-        <div>AddJob</div>
+        <form>
+            <div>
+                <p>Job Title</p>
+                <input type="text" placeholder='Type here'
+                    onChange={e => setTitle(e.target.value)} value={title} required />
+            </div>
+
+            <div>
+                <p>Job Description</p>
+                <div ref={editorRef}>
+
+                </div>
+            </div>
+        </form>
     )
 }
 
