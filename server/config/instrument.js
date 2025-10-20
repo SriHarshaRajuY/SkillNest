68105df6832e8c1d3ebed362596c4d2d542ac702
SriHarshaRajuY <sriharsharajuy@gmail.com>
2025-10-20 11:38:51 +0530

    created User Schema and started the backend part of this project

---
+++ server/config/instrument.js
@@ -0,0 +1,10 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node"
+
+Sentry.init({
+  dsn: "https://ba48b2fc4950f568454905703ee1e94c@o4510220204310528.ingest.us.sentry.io/4510220208308224",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  integrations: [Sentry.mongooseIntegration()],
+  sendDefaultPii: true,
+});
---
+++ server/controllers/webhooks.js
@@ -0,0 +1,11 @@
+import { Webhook } from "svix";
+import User from "../models/Users.js"
+
+// API Controller Function to Manage Clerk User with database
+export const clerkWebhooks = async(req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
